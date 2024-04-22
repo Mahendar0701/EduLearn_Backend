b3816f0afc13e4cf55c7a981dd77fba80680c44a
mahendar <mahendarchikkolla@gmail.com>
2024-04-22 23:34:41 +0530

    .env added

--- project/settings.py
+++ project/settings.py
@@ -122,39 +122,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'edu_learn',
-        'USER': 'postgres',
-        'PASSWORD': 'mahi123@#',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',       
-        
-    },
-    'test': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('PG_TEST_DATABASE'),
-        'USER': os.getenv('PG_USER'),
-        'PASSWORD': os.getenv('PG_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
-    
-}
-
 # DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+#     # }
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('PG_DATABASE'),
-#         'USER': os.getenv('PG_USER'),
-#         'PASSWORD': os.getenv('PG_PASSWORD'),
+#         'NAME': 'edu_learn',
+#         'USER': 'postgres',
+#         'PASSWORD': 'mahi123@#',
 #         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'PORT': '5432',       
+        
 #     },
 #     'test': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -164,7 +144,27 @@ DATABASES = {
 #         'HOST': 'localhost',
 #         'PORT': '5432',
 #     },
+    
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PG_DATABASE'),
+        'USER': os.getenv('PG_USER'),
+        'PASSWORD': os.getenv('PG_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PG_TEST_DATABASE'),
+        'USER': os.getenv('PG_USER'),
+        'PASSWORD': os.getenv('PG_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+}
 
 
 # Password validation
